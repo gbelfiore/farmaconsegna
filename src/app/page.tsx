8790c7f882f98531339cdb1cc79f1c4dcbbdd300
gBelfiore <belfiore.giovanni@gmail.com>
2025-02-06 09:57:25 +0100

    add thema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1 className="text-sm font-bold underline">Hello world!</h1>;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Ciao!</h2>
+        <p>Questo è un esempio con DaisyUI.</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Acquista</button>
+        </div>
+      </div>
+    </div>
+  );
 }
